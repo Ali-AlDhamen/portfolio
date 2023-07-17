1ff70de0c5e162ccb45f192363e530bed8bc06bc
Ali-AlDhamen <ali77dhamen@hotmail.com>
2023-07-17 20:40:23 +0300

    edit tools

--- app/Tools.tsx
+++ app/Tools.tsx
@@ -5,20 +5,47 @@ type Props = {}
 const Tools = (props: Props) => {
   return (
     <div className='w-10/12 mx-auto'>
-        <h1 className='md:text-4xl text-3xl lg:text-5xl font-bold'>Tools</h1>
+        <h1 className='text-3xl font-bold md:text-4xl lg:text-5xl'>Tools</h1>
         <p className='pt-6 text-neutral-300'>The products, apps and services I use on a daily basis for work and life.</p>
-        <div className='mt-20 grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-10'>
+        <div className='grid grid-cols-1 mt-20 lg:grid-cols-3 sm:grid-cols-2 sm:gap-10'>
             <a href="" className='border-[0.5px] border-neutral-500 block rounded-2xl my-6'>
-                <div className='py-8 bg-neutral-800 w-full text-center rounded-t-2xl'>
-                    <img className='max-w-[80px] rounded-2xl mx-auto brightness-90 contrast-125' src="planetscale.jpg" alt="" />
+                <div className='w-full py-8 text-center bg-neutral-800 rounded-t-2xl'>
+                    <img className='max-w-[80px] rounded-2xl mx-auto brightness-90 contrast-125' src="fire.png" alt="" />
                 </div>
                 <div className='p-8 py-10'>
-                    <h1 className='mb-1 font-bold'>Planetscale</h1>
-                    <p className='text-neutral-400'>My daily website&lsquo;s database platform</p>
+                    <h1 className='mb-1 font-bold'>Firebase</h1>
+                    <p className='text-neutral-400'>For My Apps Backend</p>
                 </div>
             </a>
             <a href="" className='border-[0.5px] border-neutral-500 block rounded-2xl my-6'>
-                <div className='py-8 bg-neutral-800 w-full text-center rounded-t-2xl'>
+                <div className='w-full py-8 text-center bg-neutral-800 rounded-t-2xl'>
+                    <img className='max-w-[80px] rounded-2xl mx-auto brightness-90 contrast-125' src="flutter.png" alt="" />
+                </div>
+                <div className='p-8 py-10'>
+                    <h1 className='mb-1 font-bold'>Flutter</h1>
+                    <p className='text-neutral-400'>For Creating Mobile User-Friendly Interfaces </p>
+                </div>
+            </a>
+            <a href="" className='border-[0.5px] border-neutral-500 block rounded-2xl my-6'>
+                <div className='w-full py-8 text-center bg-neutral-800 rounded-t-2xl'>
+                    <img className='max-w-[80px] rounded-2xl mx-auto brightness-90 contrast-125' src="react.png" alt="" />
+                </div>
+                <div className='p-8 py-10'>
+                    <h1 className='mb-1 font-bold'>React</h1>
+                    <p className='text-neutral-400'>For Creating Interactive User-Friendly Interfaces </p>
+                </div>
+            </a>
+            <a href="" className='border-[0.5px] border-neutral-500 block rounded-2xl my-6'>
+                <div className='w-full py-8 text-center bg-neutral-800 rounded-t-2xl'>
+                    <img className='max-w-[80px] rounded-2xl mx-auto brightness-90 contrast-125' src="supabase.png" alt="" />
+                </div>
+                <div className='p-8 py-10'>
+                    <h1 className='mb-1 font-bold'>Supabase</h1>
+                    <p className='text-neutral-400'>For Authentication & Databases</p>
+                </div>
+            </a>
+            <a href="" className='border-[0.5px] border-neutral-500 block rounded-2xl my-6'>
+                <div className='w-full py-8 text-center bg-neutral-800 rounded-t-2xl'>
                     <img className='max-w-[80px] rounded-2xl mx-auto brightness-90 contrast-125' src="icon-figma.png" alt="" />
                 </div>
                 <div className='p-8 py-10'>
@@ -27,7 +54,7 @@ const Tools = (props: Props) => {
                 </div>
             </a>
             <a href="" className='border-[0.5px] border-neutral-500 block rounded-2xl my-6'>
-                <div className='py-8 bg-neutral-800 w-full text-center rounded-t-2xl'>
+                <div className='w-full py-8 text-center bg-neutral-800 rounded-t-2xl'>
                     <img className='max-w-[80px] rounded-2xl mx-auto brightness-90 contrast-125' src="vscode.png" alt="" />
                 </div>
                 <div className='p-8 py-10'>
@@ -36,7 +63,7 @@ const Tools = (props: Props) => {
                 </div>
             </a>
             <a href="" className='border-[0.5px] border-neutral-500 block rounded-2xl my-6'>
-                <div className='py-8 bg-neutral-800 w-full text-center rounded-t-2xl'>
+                <div className='w-full py-8 text-center bg-neutral-800 rounded-t-2xl'>
                     <img className='max-w-[80px] rounded-2xl mx-auto brightness-90 contrast-125' src="vercel.png" alt="" />
                 </div>
                 <div className='p-8 py-10'>
@@ -45,7 +72,7 @@ const Tools = (props: Props) => {
                 </div>
             </a>
             <a href="" className='border-[0.5px] border-neutral-500 block rounded-2xl my-6'>
-                <div className='py-8 bg-neutral-800 w-full text-center rounded-t-2xl'>
+                <div className='w-full py-8 text-center bg-neutral-800 rounded-t-2xl'>
                     <img className='max-w-[80px] rounded-2xl mx-auto brightness-90 contrast-125' src="dribbble.png" alt="" />
                 </div>
                 <div className='p-8 py-10'>
