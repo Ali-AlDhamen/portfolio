955d70e15738226981031b6e2c7b20bf2440f0d9
Ali-AlDhamen <ali77dhamen@hotmail.com>
2023-07-17 20:40:08 +0300

    edit projects

--- app/Container.tsx
+++ app/Container.tsx
@@ -20,7 +20,7 @@ const Container = (props: Props) => {
             </div>
             <div className='p-4 pl-0 mt-5 sm:mt-20'>
                 <p className='text-xl sm:text-2xl md:text-xl lg:text-2xl xl:text-3xl'>Hello</p>
-                <h1 className='pt-2 text-4xl sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl lg:pt-6'>I&apos;m Ali<p className='py-4 text-xl sm:text-2xl md:text-xl lg:text-2xl xl:text-3xl'>A Software Engineer </p></h1>
+                <h1 className='pt-2 text-4xl sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl lg:pt-6'>I&apos;m Ali<p className='py-4 text-xl sm:text-2xl md:text-xl lg:text-2xl xl:text-3xl'>A Frontend Developer </p></h1>
                 <p className='text-base sm:text-xl md:text-base lg:text-xl xl:text-2xl lg:pt-6'>bringing imagination to live</p>
                 {/* <div className='pt-10'>
                     <p className='inline-block px-3 py-1 shadow-md before:bg-white bg-slate-400'>Hire</p>
@@ -35,7 +35,7 @@ const Container = (props: Props) => {
                 <div className='flex items-center mt-6 md:mt-14'>
                     <ul className='flex items-center my-4 mr-6 space-x-2 sm:space-x-5 '>
                         <li><Link href="https://twitter.com/_AliDhamen" target="_blank" rel="noreferrer" className='transition duration-150 ease-in opacity-75 hover:opacity-100'><AiOutlineTwitter size={24}/></Link></li>
-                        <li><Link href="https://linkedin.com/in/ali-aldhamen" target="_blank" rel="noreferrer" className='transition duration-150 ease-in opacity-75 hover:opacity-100'><AiOutlineLinkedin size={24}/></Link></li>
+                        <li><Link href="https://www.linkedin.com/in/ali-al-dhamen-323a04144" target="_blank" rel="noreferrer" className='transition duration-150 ease-in opacity-75 hover:opacity-100'><AiOutlineLinkedin size={24}/></Link></li>
                         <li><Link href="https://github.com/Ali-AlDhamen" target="_blank" rel="noreferrer" className='transition duration-150 ease-in opacity-75 hover:opacity-100'><AiOutlineGithub size={24}/></Link></li>
                     </ul>
                     <a href="mailto:ali77dhamen@hotmail.com" className='px-4 py-[5px] whitespace-nowrap transition ease-in duration-150 bg-neutral-700 hover:bg-neutral-600 rounded-2xl border-[0.5px] border-[#363636]'>Email me</a>
--- app/Projects.tsx
+++ app/Projects.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, {useState} from 'react'
 import {BsArrowUpRight} from "react-icons/bs"
+import Link from 'next/link';
 
 type Props = {}
 
@@ -8,31 +9,39 @@ const Projects = (props: Props) => {
     const [selectedId, setSelectedId] = useState<boolean>(false)
   return (
     <div className='w-10/12 mx-auto'>
-        <h1 className='md:text-4xl text-3xl lg:text-5xl font-bold'>Projects</h1>
-        <p className='pt-6 text-neutral-300'>Over the years, I have gained experience in designing and developing a variety of websites, ranging from simple one-page landing pages to complex multi-page applications.Here are some projects that I would like to present to all of you, showcasing my work and achievements in the field of web design and development.</p>
+        <h1 className='text-3xl font-bold md:text-4xl lg:text-5xl'>Projects</h1>
+        <p className='pt-6 text-neutral-300'>Over the years, I have gained experience in designing and developing a variety of websites and applications, ranging from simple one-page landing pages to complex multi-page applications.Here are some projects that I would like to present to all of you, showcasing my work and achievements in the field of web development and app development.</p>
         {/* <div className='w-10/12 box1 max-md:mt-[-50px] mt-0'></div> */}
-        <div className='mt-20 md:w-9/12 mx-auto '>
-            <img className='rounded-xl opacity-90' src="project.png" alt="" />
-            <div className='my-10 sm:w-10/12 mx-auto'>
-                <h1 className='text-2xl font-bold mb-4'>Landing Page</h1>
-                <div className='text-neutral-300'>A landing page for designers made with basic html, css with great color combination.</div>
-                <div className='mt-6 inline-block'><a href="" className='flex items-center px-4 py-1 border-[0.5px] border-slate-500 rounded-3xl bg-neutral-800 hover:bg-neutral-700'>Explore <BsArrowUpRight size={14} className='ml-2'/></a></div>
+        <div className='mx-auto mt-20 md:w-9/12 '>
+            <img className='rounded-xl opacity-90' src="gforcey.png" alt="" />
+            <div className='mx-auto my-10 sm:w-10/12'>
+                <h1 className='mb-4 text-2xl font-bold'>GForce-RT</h1>
+                <div className='text-neutral-300'>Discover the world of GForce-RT, a cutting-edge website showcasing their exceptional work and top-notch services. Built with Next.js and Tailwind CSS</div>
+                <div className='inline-block mt-6'><Link href="https://www.gforce-rt.com/" target="_blank" rel="noreferrer"  className='flex items-center px-4 py-1 border-[0.5px] border-slate-500 rounded-3xl bg-neutral-800 hover:bg-neutral-700'>Explore <BsArrowUpRight size={14} className='ml-2'/></Link></div>
             </div>
         </div>
-        <div className='mt-20 md:w-9/12 mx-auto '>
-            <img className='rounded-xl opacity-90' src="project3.png" alt="" />
-            <div className='my-10 sm:w-10/12 mx-auto'>
-                <h1 className='text-2xl font-bold mb-4'>React:News Website</h1>
-                <div className='text-neutral-300'>A news website using the React framework, which continuously provides the latest updates and discussions from around the world. <h1 className='mt-4'> This dynamic platform utilizes real-time data fetching from the Yahoo News API, ensuring that users are constantly informed with up-to-date news content.</h1></div>
-                <div className='mt-6 inline-block'><a href="" className='flex items-center px-4 py-1 border-[0.5px] border-slate-500 rounded-3xl bg-neutral-800 hover:bg-neutral-700'>Explore <BsArrowUpRight size={14} className='ml-2'/></a></div>
+        <div className='mx-auto mt-20 md:w-9/12 '>
+            <img className='rounded-xl opacity-90' src="taqyeemi.png" alt="" />
+            <div className='mx-auto my-10 sm:w-10/12'>
+                <h1 className='mb-4 text-2xl font-bold'>Taqyeemi App</h1>
+                <div className='text-neutral-300'>A student-centric app empowering users to rate college courses, evaluate instructors, and provide valuable feedback for future students. Enhance your learning journey With access to a wealth of educational resources. Bulit with Flutter and Firebase</div>
+                <div className='inline-block mt-6'><Link href="https://github.com/Ali-AlDhamen/taqyeemi" target="_blank" rel="noreferrer"  className='flex items-center px-4 py-1 border-[0.5px] border-slate-500 rounded-3xl bg-neutral-800 hover:bg-neutral-700'>Explore <BsArrowUpRight size={14} className='ml-2'/></Link></div>
             </div>
         </div>
-        <div className='mt-20 md:w-9/12 mx-auto '>
-            <img className='rounded-xl opacity-90' src="project1.png" alt="" />
-            <div className='my-10 sm:w-10/12 mx-auto'>
-                <h1 className='text-2xl font-bold mb-4'>Hotel Booking App</h1>
-                <div className='text-neutral-300'>The hotel booking app is built with Next.js and utilizes Tailwind CSS for styling. It integrates MongoDB for database management and Prisma Cloudinary for efficient image storage. <h1 className='mt-4'>This combination of technologies enables seamless and user-friendly hotel booking experiences, delivering a robust and visually appealing application.</h1></div>
-                <div className='mt-6 inline-block'><a href="" className='flex items-center px-4 py-1 border-[0.5px] border-slate-500 rounded-3xl bg-neutral-800 hover:bg-neutral-700'>Explore <BsArrowUpRight size={14} className='ml-2'/></a></div>
+        <div className='mx-auto mt-20 md:w-9/12 '>
+            <img className='rounded-xl opacity-90' src="aklne.png" alt="" />
+            <div className='mx-auto my-10 sm:w-10/12'>
+                <h1 className='mb-4 text-2xl font-bold'>Aklne App</h1>
+                <div className='text-neutral-300'>Mobile application that connect individuals and businesses with surplus food to those who are in need. It aims to reduce food waste and help alleviate hunger and poverty. Bulit With Flutter and Firebase</div>
+                <div className='inline-block mt-6'><Link href="https://github.com/Radwan-Albahrani/Akalne" target="_blank" rel="noreferrer"  className='flex items-center px-4 py-1 border-[0.5px] border-slate-500 rounded-3xl bg-neutral-800 hover:bg-neutral-700'>Explore <BsArrowUpRight size={14} className='ml-2'/></Link></div>
+            </div>
+        </div>
+        <div className='mx-auto mt-20 md:w-9/12 '>
+            <img className='rounded-xl opacity-90' src="quizo.png" alt="" />
+            <div className='mx-auto my-10 sm:w-10/12'>
+                <h1 className='mb-4 text-2xl font-bold'>QuizAI App</h1>
+                <div className='text-neutral-300'>Mobile application for personalized quizzes. you can effortlessly generate quizzes from text or files, or dive into exciting quizzes created by others. Built With Flutter and Firebase (Still Cooking)</div>
+                <div className='inline-block mt-6'><Link href=""  className='flex items-center px-4 py-1 border-[0.5px] border-slate-500 rounded-3xl bg-neutral-800 hover:bg-neutral-700'>Explore <BsArrowUpRight size={14} className='ml-2'/></Link></div>
             </div>
         </div>
     </div>
