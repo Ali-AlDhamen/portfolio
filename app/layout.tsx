46168ccbb698de108cbd37b4622e57c36187fbdc
Ali-AlDhamen <ali77dhamen@hotmail.com>
2023-07-17 21:11:02 +0300

    fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ const space_mono = Space_Mono({ weight: ["400", "700"], subsets: ["latin"] });
 
 export const metadata = {
   title: "Ali AlDhamen",
-  description: "Software Engineer Based In Saudi Arabia",
+  description: "App/Web Developer Based In Saudi Arabia",
   keywords: [
     "Software Engineer",
     "Web Developer",
