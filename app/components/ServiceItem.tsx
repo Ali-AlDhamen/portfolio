d9de7bb8714ef90b285c5aee1325c9fc900d52ee
Ali-AlDhamen <ali77dhamen@hotmail.com>
2023-07-19 15:38:42 +0300

    finally fixed

--- app/components/ServiceItem.tsx
+++ app/components/ServiceItem.tsx
@@ -1,19 +1,6 @@
 import { FC } from "react";
 import { ServiceItemProps } from "../types/types";
-import "../globals.css";
 
-const findColor = (color: string) => {
-  switch (color) {
-    case "blue-600":
-      return "bg-blue-600";
-    case "pink-600":
-      return "bg-pink-600";
-    case "yellow-600":
-      return "bg-yellow-600";
-    default:
-      return "bg-green-600";
-  }
-};
 
 const ServiceItem: FC<ServiceItemProps> = ({
   title,
@@ -21,10 +8,10 @@ const ServiceItem: FC<ServiceItemProps> = ({
   Icon,
   color,
 }) => {
-  const bgColor = findColor(color);
+
 
   return (
-    <div className="p-5 mt-5 overflow-hidden md:w-1/3 d relativ">
+    <div className="relative p-5 mt-5 overflow-hidden md:w-1/3 d">
       <h1 className="mt-5 text-center">{title}</h1>
       <div className="flex justify-center mt-4">
         <Icon className={`text-${color}`} size={28} />
@@ -43,7 +30,7 @@ const ServiceItem: FC<ServiceItemProps> = ({
       </p>
       <span className="w-[60px] h-[60px] z-[-2] blur-[100px] left-[45%] bg-white absolute top-0 "></span>
       <span
-        className={`w-[20px] h-[20px] z-[-2] blur-[20px] left-[48%]  absolute top-[27%] ${bgColor} bg-blue-600`}
+        className={`w-[20px] h-[20px] z-[-2] blur-[20px] left-[48%]  absolute top-[27%] bg-${color}`}
       ></span>
       <span className="w-[50px] h-[50px] z-[-2] blur-[70px] right-0 bg-blue-600 absolute bottom-0 "></span>
       <span className="w-[25px] h-[25px] z-[-2] blur-[40px] top-[75%] left-[40%] bg-green-600 absolute bottom-0 "></span>
