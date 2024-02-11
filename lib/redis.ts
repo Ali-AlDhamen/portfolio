dfeeb9211d1a43de892534a556ce5e59b0538509
Ali-AlDhamen <ali77dhamen@hotmail.com>
2024-02-12 01:33:28 +0300

    change redis key

--- lib/redis.ts
+++ lib/redis.ts
@@ -2,5 +2,5 @@ import { Redis } from '@upstash/redis'
 
 export const redis = new Redis({
   url: 'https://us1-ideal-gannet-39020.upstash.io',
-  token: 'AZhsASQgZjUxOWNlY2EtNmRhMC00OTY4LWEyNjctOTA5NWJjMWMxNDFkYWNmMWJmNDU1YTdmNGEyYjk5YTMwOTBiNGEzOTI4OGM=',
+  token: process.env.REDIS_TOKEN,
 })
